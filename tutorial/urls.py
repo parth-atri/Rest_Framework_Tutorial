2390dc6923bcd55e4cbe0644fa41fd6d1107f491
Parth Shah <shahpv@ATRI003362.local>
2025-07-17 15:10:48 -0700

    feat(Rest_Framework_Tutorial-Chapter1): Serialization
    Added serializers and views to use them.
    
    [ICTRAINING-2188]

--- tutorial/urls.py
+++ tutorial/urls.py
@@ -16,17 +16,7 @@ Including another URLconf
 """
 
 from django.urls import path, include
-from tutorial.quickstart import views
-from rest_framework import routers
-
-router = routers.DefaultRouter()
-router.register(r"users", views.UserViewSet)
-router.register(r"groups", views.GroupViewSet)
-
-# Wire up our API using automatic URL routing.
-# Additionally, we include login URLs for the browsable API.
 
 urlpatterns = [
-    path("", include(router.urls)),
-    path("api-auth/", include("rest_framework.urls", namespace="rest_framework")),
+    path("", include("snippets.urls")),
 ]
